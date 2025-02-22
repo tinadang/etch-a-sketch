6dc42a7ea0370e3c2175709fb669f7bd8782f55e
Tina Dang <tdtinadang@gmail.com>
2025-02-21 20:13:17 -0800

    Add function to get user input, delete a grid

--- javascript.js
+++ javascript.js
@@ -1,7 +1,18 @@
 
+function addNewGrid() {
+    const squaresPerSide = prompt("How many squares per side for your new grid?");
+
+    deleteCurrentGrid();
+    // addSquares(squaresPerSide);
+}
+
+function deleteCurrentGrid() {
+    squares.forEach((square) => container.removeChild(square));
+}
+
 //Insert nth number of squares into the container
-function addSquares(numberOfSquares) {
-    for (let i = 0; i < numberOfSquares; i++) {
+function addSquares(squaresPerSide) {
+    for (let i = 0; i < Math.pow(squaresPerSide, 2); i++) {
         const newSquare = document.createElement("div");
         newSquare.className = "square";
         container.appendChild(newSquare);
@@ -13,9 +24,14 @@ function applyHover(event) {
     event.target.style.backgroundColor = "lightgray";
 }
 
+
 //Add nth number of squares to container
 const container = document.querySelector(".container");
-addSquares(256);
+let squaresPerSide = 16;
+addSquares(squaresPerSide);
+
+const adjustSquaresButton = document.querySelector("button");
+adjustSquaresButton.addEventListener("click", addNewGrid);
 
 //Apply a hover action to all squares
 const squares = document.querySelectorAll(".square");
